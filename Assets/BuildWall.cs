134fbdac7f02cbb04990a8860053e72b208bd34d
Todd Smith <elliptic1@gmail.com>
2013-08-08 14:29:06 -0400

    First commit

---
+++ Assets/BuildWall.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildWall : MonoBehaviour {
+
+    GameObject[] wallBlock;
+
+	// Use this for initialization
+	void Start () {
+
+ //       wallBlock[0] = GameObject.CreatePrimitive(PrimitiveType.Cube);
+ //       wallBlock[0].transform.position = new Vector3(10, 10, 10);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Fire.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fire : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.touchCount > 0)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                rigidbody.AddForce(new Vector3(100, 0, 0));
+            }
+        }
+	
+	}
+}
